889a508c85e185e8bc925bc3ef10a43b86687dc2
pkorycin <pkorycin>
2019-11-12 21:31:20 +0100

    Cleanup little bit and integrate with project

--- client_main.cpp
+++ client_main.cpp
@@ -1,18 +1,15 @@
 #include "ZeroMqUtils.hpp"
 #include <iostream>
-#include <unistd.h>
+#include <thread>
 
 int main () {
-    //  Prepare our context and socket
     zmq::context_t context (1);
     zmq::socket_t socket (context, ZMQ_REP);
-    socket.bind ("ipc:///tmp/test");
-
+    socket.bind ("ipc:///home/pkorycin/my_port");
     while (true) {
         std::string message = s_recv(socket);
         std::cout << "Received Message: " << message << std::endl;
-        //  Do some 'work'
-        sleep (1);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
         s_send(socket, "WorldOfDupa");
     }
     return 0;
--- server_main.cpp
+++ server_main.cpp
@@ -3,14 +3,11 @@
 
 int main (void)
 {
-  zmq::context_t context(1);
-
-    printf ("Connecting to hello world server…\n");
+    zmq::context_t context(1);
     zmq::socket_t socket (context, ZMQ_REQ);
-    socket.connect("ipc:///tmp/test");
-
+    socket.connect("ipc:///home/pkorycin/my_port");
     for (int request_nbr = 0; request_nbr != 10; request_nbr++) {
-       s_send(socket, "DUPAA");
+       s_send(socket, "DUPAAA");
        std::string receivedMessage = s_recv(socket);
        std::cout << request_nbr << " - Received Message: " << receivedMessage << std::endl;
     }
